0417996d8dd5e83a5250117e464234c72da09a62
adrien-dubois <mcsnoos@gmail.com>
2022-05-29 10:03:20 +0200

    [ADD]Primary users crud

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,7 +10,7 @@ module.exports.getAllUsers = async (req, res) => {
 /*----- GET ONE USER BY ITS ID WITHOUT PASSWORD -----*/
 module.exports.getUser = (req, res) => {
     if (!ObjectId.isValid(req.params.id))
-        return res.status(400).json('ID unknown : ' + req.params.id)
+        return res.status(404).json('ID unknown : ' + req.params.id)
 
     UserModel.findById(req.params.id, (err, data) => {
         if(!err) res.status(200).json(data);
@@ -24,19 +24,35 @@ module.exports.updateUser = async (req, res) => {
     const { id: _id } = req.params;
     const bio = req.body.bio;
 
-    if (!ObjectId.isValid(_id)) return res.status(404).send('ID unknown');
-
     try{
-        await UserModel.findOneAndUpdate(
+        const updateUser = await UserModel.findByIdAndUpdate(
             { _id },
             {
                 $set: { bio }
             },
-            {new: true, upsert: true, setDefaultsOnInsert: true});
+            { new: true, runValidators: true });
+        if(!updateUser){
+            return res.status(404).json({ message: "L'utilisateur " + req.params.id + " n'existe pas" });
+        }
 
-        return res.status(200).json("L'utilisateur a bien été mis à jour");
+        res.status(200).json("L'utilisateur a bien été mis à jour");
 
-    } catch(err) {
-        return res.status(500).json({ message: err });
+    } catch(e) {
+        return res.status(500).json({ message: e });
     }
 };
+
+exports.deleteUser = async (req, res) => {
+
+    try{
+        const user = await UserModel.findByIdAndDelete({_id: req.params.id})
+
+        if(!user){
+            return res.status(404).json({ message: "L'utilisateur " + req.params.id + " n'existe pas"});
+        }
+
+        res.status(200).json({ message: "L'utilisateur a bien été supprimé"});
+    } catch (e) {
+        res.status(500).json({ message: e });
+    }
+}
